bb24f13bc3fd321fe67e2a0876af036769044faf
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-06-30 15:05:24 +0200

    add home view model

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:goal_buddy_riverpod/data/goal.dart';
+import 'package:goal_buddy_riverpod/viewmodel/home_viewmodel.dart';
 import 'package:goal_buddy_riverpod/widgets/goal_card.dart';
 
-class HomeView extends StatelessWidget {
-  final goals = [Goal(name: "test", description: "description", color: Colors.purple)];
-
+class HomeView extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('Goal Tracky'),
@@ -19,11 +19,31 @@ class HomeView extends StatelessWidget {
           child: Icon(Icons.add),
         ),
       ),
-      child: ListView.builder(
-        itemCount: goals.length,
-        itemBuilder: (context, index) {
-          final goal = goals[index];
-          return GoalCard(goal: goal);
+      child: Consumer(
+        builder: (context, watch, _) {
+          final viewModel = ref.watch(homeScreenViewModelProvider);
+          final goals = viewModel.goals;
+
+          return ListView.builder(
+            itemCount: goals.length,
+            itemBuilder: (context, index) {
+              final goal = goals[index];
+
+              return Card(
+                elevation: 3,
+                child: CupertinoListTile(
+                  title: Text(goal.name),
+                  subtitle: Text(goal.description),
+                  trailing: CupertinoButton(
+                    child: Icon(Icons.delete),
+                    onPressed: () {
+                      viewModel.deleteGoal(goal);
+                    },
+                  ),
+                ),
+              );
+            },
+          );
         },
       ),
     );
